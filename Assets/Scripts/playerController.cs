3418b60a327c6d7c69e7e7a344526b5e4da9dc35
drumalv <alvarouniversidad98@gmail.com>
2018-10-06 01:37:18 +0200

    Movimiento nave
    He eliminado algunos scripts problematicos al compilar y he programado el movimiento de la nave.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,16 +1,31 @@
-﻿    using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
+[System.Serializable]
+public class Boundary
+{
+    public float xMax, xMin, zMax, zMin;
+}
 
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rig;
+    public float speed, tilt;
+    public Boundary boundary;
+    
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        rig = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rig.velocity = movement*speed;
+        rig.position = new Vector3(Mathf.Clamp(rig.position.x, boundary.xMax, boundary.xMin), 0f, Mathf.Clamp(rig.position.z, boundary.zMax, boundary.zMin));
+        rig.rotation = Quaternion.Euler(0f, 0f, rig.velocity.x * -tilt);
 	}
 }
